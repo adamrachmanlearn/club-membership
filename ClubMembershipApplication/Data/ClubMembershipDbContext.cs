be86b74188d97cf3c835d1b23411744900d08e2d
Adam <adamrachman.learn@gmail.com>
2025-06-05 13:33:08 +0700

    Add db context class

---
+++ ClubMembershipApplication/Data/ClubMembershipDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClubMembershipApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClubMembershipApplication.Data
+{
+    public class ClubMembershipDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
